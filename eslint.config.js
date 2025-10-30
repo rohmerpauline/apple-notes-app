942d89ccc8786b118bb3129643491ac767fa7fbb
rohmerpauline <rohmer.pauline@outlook.com>
2025-10-30 15:51:47 +0100

    chore: install librairies and setup code linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    plugins: ['prettier'],
+    extends: ['prettier'],
+    ignores: ['dist/*', 'node_modules/**'],
+    rules: { 'prettier/prettier': 'warn' },
   },
 ]);
